59854d0b75668d84ffce0f8f85ead7bafb483c43
adarsh singh <adsredmi182615@gmail.com>
2026-01-26 13:03:41 +0530

    frontend made

--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -1,48 +1,48 @@
 "use client";
-
-import Editor from "@monaco-editor/react";
 import { useState } from "react";
 import type { SupportedLanguage } from "@/types/language";
-
+import AiResponsePage from "./ResponseModal";
 interface CodeEditorProps {
   language: SupportedLanguage;   // selected language from parent
   onSubmit: (code: string) => void;
 }
 
 export default function CodeEditor({ language, onSubmit }: CodeEditorProps) {
-  const [code, setCode] = useState<string>(""); // blank editor
-
+  const [errorText, setErrorText] = useState<string>("");
+  const [data, setData] = useState<string>("");
   const test = async () => {
-  const res = await fetch("/api/stacktrace", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      code,
-    }),
-  });
+    const res = await fetch("/api/stacktrace", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        errorText,
+      }),
+    });
 
-  const data = await res.json();
-  console.log(data);
-};
+    const json = await res.json();
+    const aiText =
+      json.result?.candidates?.[0]?.content?.parts?.[0]?.text ?? "";
+    setData(aiText)
+  };
 
   return (
     <div className="flex flex-col">
-      <textarea name="" id="" className="border-2 " onChange={(e)=>{
-        setCode(e.target.value)
+      <textarea name="" id="" className="border-2 " onChange={(e) => {
+        setErrorText(e.target.value)
       }}></textarea>
       <div className="overflow-hidden  px-0 pt-2 py-2">
         <button
-        onClick={test}
-        
-        className=" bg-blue-600 hover:bg-blue-700 disabled:bg-gray-600 px-4 py-2 rounded"
-      >
-        Debug Code
-      </button>
+          onClick={test}
+
+          className=" bg-blue-600 hover:bg-blue-700 disabled:bg-gray-600 px-4 py-2 rounded"
+        >
+          Debug Code
+        </button>
       </div>
+      <AiResponsePage data={data} />
     </div>
   );
 }
 
-// in this i will hit the backend endpoint
---
+++ components/ResponseModal.tsx
@@ -0,0 +1,10 @@
+"use client"
+export default function AiResponsePage({data}:{
+    data:string
+}){
+    return(
+        <div>
+            {data}
+        </div>
+    )
+}
